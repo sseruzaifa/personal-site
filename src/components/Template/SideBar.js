9d2946839dced13f152aa4fc8f15b178b94a91ad
Huzaifa Sserugo <sseruzaifa@gmail.com>
2023-06-10 00:20:31 +0300

    img

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,17 +9,17 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/huza.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a></p>
+        <h2>Huzaifa Sserugo</h2>
+        <p><a href="mailto:michael@mldangelo.com">huzaifa@greatnes.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
+      <p>Hi, I&apos;m Huzaifa. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni,
         and the VP of Engineering at <a href="https://smileidentity.com">Smile Identity</a>. Previously,
         I was the co-founder and CTO of <a href="https://arthena.com">Arthena</a>
         , co-founder of <a href="https://matroid.com">Matroid</a>, and worked at
