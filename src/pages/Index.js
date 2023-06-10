2d444e170a0585ae08f860cf1bb7669f5ec00bbb
Huzaifa Sserugo <sseruzaifa@gmail.com>
2023-06-10 09:56:06 +0300

    more setup changes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,8 +5,8 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
+    description={"Huzaifa Sserugo's personal website. Kampala based Makerere Electrical Engineering graduate, "
+    + 'Service Engineer at Mantrac Uganda Limited, currently deployed in Buliisa in the Total-Tilenga project'}
   >
     <article className="post" id="index">
       <header>
@@ -25,7 +25,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/sseruzaifa/personal-site">here</a>.</p>
     </article>
   </Main>
 );
